b80f25b5346af43e4da281b9a117acd2e1cf6cd5
mahato861 <55026761+mahato861@users.noreply.github.com>
2019-10-15 13:43:16 -0400

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-
+// this is my firsht dev project
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!. This is a page maintained by Manoj";
+      
     }
 
 }
